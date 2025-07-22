fbc39132d5feb8f6e2574f4ff2bc1c21f3f6c10f
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-07-22 15:24:16 +0100

    init project

---
+++ main.py
@@ -0,0 +1,20 @@
+from models.models import SessionLocal, engine, Base
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+
+app = FastAPI()
+Base.metadata.create_all(bind=engine)
+
+def Get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+@app.get("/health")
+def get_health():
+    return {"sucess": 200}
