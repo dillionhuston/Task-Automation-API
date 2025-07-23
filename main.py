380d39c9e3dd473245a0039250d14077a7d70655
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-07-23 12:33:36 +0100

    Add User model

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from models.models import SessionLocal, engine, Base
+from models.database import SessionLocal, engine, Base
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
