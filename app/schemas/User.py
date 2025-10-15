99edc77f733b41990587ed0d22d1d2b0c8ff2d39
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-10-15 17:15:17 +0100

    Update User.py

--- app/schemas/User.py
+++ app/schemas/User.py
@@ -15,13 +15,15 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: Annotated[str, StringConstraints(min_length=8)]
     username: Annotated[str, StringConstraints(min_length=3, max_length=50)]
-    id: str
+    is_admin: bool = False  # optional, defaults to False
 
 # pylint: disable=too-few-public-methods
 class User(BaseModel):
     """Schema representing a user."""
+    id: str
     username: str
     email: EmailStr
+    is_admin: bool
 
     class Config:
         """Pydantic config to allow attribute access."""
