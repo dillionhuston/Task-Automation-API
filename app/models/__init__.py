55823319eba46152ab6b19237c2cac9a7ef3a8c5
Dillion Huston <122535848+dillionhuston@users.noreply.github.com>
2025-11-23 12:31:47 +0000

    Update imports to use relative paths in models and routers
    Changed absolute imports to relative imports in app/models/__init__.py and app/routers/__init__.py for consistency and to support package structure.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,6 +5,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
 from sqlalchemy import Column, String, Integer
-from app.models.database import Base
-from app.models.user import UserModel
-from app.models.file import FileModel
+from ..models.database import Base
+from ..models.user import UserModel
+from ..models.file import FileModel
--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -2,8 +2,8 @@
 Make routers a subpackage of app.
 """
 
-from app.routers.auth import register, read_current_user
-from app.routers.files import upload_file, list_files, delete_file
+from ..routers.auth import register, read_current_user
+from ..routers.files import upload_file, list_files, delete_file
 # from .tasks import schedule_task_endpoint, list_tasks_endpoint, cancel_task_endpoint
 
 __all__ = [
